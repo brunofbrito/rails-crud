de576a6dee2f62f78f2b9b91c999aa45b16e1c5d
Bruno Brito <brunofbrito@gmail.com>
2015-07-30 22:25:24 +0100

    Installed Clearance and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730194542) do
+ActiveRecord::Schema.define(version: 20150730212334) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,18 @@ ActiveRecord::Schema.define(version: 20150730194542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.boolean  "admin",                          default: false, null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128,                 null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
