e2f674567f5477df97ffedc694bbce8a279262ff
Bruno Brito <brunofbrito@gmail.com>
2015-07-30 22:04:54 +0100

    Added CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.update(params[:id], post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      # It's mandatory to specify the nested attributes that should be whitelisted.
+      # If you use `permit` with just the key that points to the nested attributes hash,
+      # it will return an empty hash.
+      params.require(:post).permit(:title, :body)
+    end
+end
